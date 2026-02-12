f168708c3f2314a6741d626b81f86249d78a2c57
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2026-02-12 17:35:09 +0000

    password generator

---
+++ 05_passwordgenerator/src/App.jsx
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numAlw, setNumAlw] = useState(false)
+  const [charAlw, setCharAlw] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const passwordGenerator = useCallback(() => {
+    let pass= ''
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if(numAlw){
+      str = str + '0123456789'
+    }
+    if(charAlw) {
+      str = str + "!@#$%^&*-_+=[]{}~`"
+    }
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = pass + str.charAt(char)
+    }
+    setPassword(pass)
+
+  }, [length, numAlw, charAlw, setPassword])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numAlw, charAlw, passwordGenerator])
+
+  return (
+    <>
+    <div className='container'>
+      <div className='form-wrap'>
+        <div className="input-group">
+          <input type="text" className="form-control" placeholder="Username" value={password} />
+          <button className='btn btn-primary' type="button">Copy</button>
+        </div>
+        <div className='other-fields'>
+          <div className='form-check'>
+            <input type="range" className="form-range" id="range1" min={6} max={100} value={length} onChange={(e)=> {setLength(e.target.value)}}></input>
+            <label for="range1" className="form-label">Password Length ({length})</label>
+          </div>
+          <div className="form-check">
+          <input className="form-check-input" type="checkbox" id="checkDefault" onChange={() => {setNumAlw((prev) => !prev)}} />
+          <label className="form-check-label" for="checkDefault">Number Allow</label>
+        </div>
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" id="checkChecked" onChange={() => {setCharAlw((prev) => !prev)}} />
+          <label className="form-check-label" for="checkChecked">Character Allow</label>
+        </div>
+        </div>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
