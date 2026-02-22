2031adba241e2f5bb2d8002cf2cb9644dd04d959
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2026-02-22 15:54:37 +0000

    react router

---
+++ 07_reactrouter/src/App.jsx
@@ -0,0 +1,63 @@
+
+import './App.css'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+import Github, {githubInfoLoader} from './components/GitHub/Github'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       },
+//       {
+//         path: 'user/:userid',
+//         element: <User />
+//       },
+//       {
+//         path: 'github',
+//         element: <Github />,
+//         loader: githubDataInformation
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+))
+
+function App() {
+
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+export default App
