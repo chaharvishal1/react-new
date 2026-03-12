acd3ecfd842fbfc1d3b7d3168be2974eff601af1
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2026-03-12 17:13:38 +0000

    push changesrelated to mega project

---
+++ 12_megaproject/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteURL : String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+    appwriteDatabase : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollection : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucket : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
