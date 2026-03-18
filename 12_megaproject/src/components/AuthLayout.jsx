1d4265e85f04e4f9b6bc1b37293071500be87475
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2026-03-18 18:23:49 +0000

    commit to mega project

---
+++ 12_megaproject/src/components/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const Protected = ({ children, authentication = true }) => {
+
+  const navigate = useNavigate()
+  const authStatus = useSelector(state => state.auth.status)
+  const [loader, setLoader] = useState(true)
+
+  useEffect(() => {
+    if(authentication && authStatus !== authentication){
+        navigate("/login")
+    }
+    else if(!authentication & authStatus !== authentication){
+        navigate("/")
+    }
+    setLoader(false)
+  }, [authentication, navigate, authStatus])
+
+  return (
+    loader ? <h1>Loading...</h1> : <>{children}</>
+  );
+};
+
+export default Protected;
---
+++ 12_megaproject/src/components/Signup.jsx
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import {Input, Button, Logo} from './index'
+import { useForm } from 'react-hook-form';
+import { authService } from '../appwrite/auth';
+import { login } from '../store/authSlice';
+
+const Signup = ({ props }) => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const {register, handleSubmit} = useForm()
+  const [error, setError] = useState('')
+
+  const create = async(data)=> {
+    setError('')
+    try {
+        const userData = authService.createAccount()
+        if(userData){
+            const userStatus = authService.getCurrentUser()
+            if(userStatus) dispatch(login(userStatus))
+            navigate('/')
+        }
+    } catch (error) {
+        setError(error.message)
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center">
+        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <div className="mb-2 flex justify-center">
+                <span className="inline-block w-full max-w-[100px]">
+                    <Logo width="100%" />
+                </span>
+            </div>
+            <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+            <p className="mt-2 text-center text-base text-black/60"> Already have an account?&nbsp;
+                <Link
+                    to="/login"
+                    className="font-medium text-primary transition-all duration-200 hover:underline"
+                >Sign In</Link>
+            </p>
+            {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+            <form onSubmit={handleSubmit(create)}>
+                <Input 
+                    label="name"
+                    placeholder="Enter your name"
+                    type="text"
+                    {...register("name", {
+                        required: true
+                    })}
+                />
+                <Input 
+                    label="email"
+                    placeholder="Enter your email"
+                    type="email"
+                    {...register("email", {
+                        required:true,
+                        validate: {
+                            matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                            "Email address must be a valid address",
+                        }
+                    })}
+                />
+                <Input 
+                    label="password"
+                    placeholder="Enter password"
+                    type="text"
+                    {...register("password", {
+                        required: true
+                    })}
+                />
+                <Button type="submit" className="w-full">Create Account</Button>
+            </form>
+            </div>
+        </div>    
+  );
+};
+
+export default Signup;
